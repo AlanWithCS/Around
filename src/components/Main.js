8f743a4d021629ae07770e1aa5ca26a294bd5827
Zhiying Qian <zhiyingq@uci.edu>
2018-12-16 14:50:35 -0800

    react-router

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Register} from "./Register";
-import { Login } from './Login'
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { Login } from './Login';
+import { Link } from 'react-router-dom';
 
 export class Main extends React.Component {
+    getLogin = () => {
+        return <Redirect to='/login'/>
+    }
     render() {
         return (
             <div className="main">
-                <Login  />
-                <Register/>
+                <Switch>
+                    <Route exact path='/' render={this.getLogin} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/register' component={Register} />
+                    <Route render={this.getLogin}/>
+                </Switch>
+
             </div>
         )
     }
